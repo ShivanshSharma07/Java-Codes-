e3167b42bb7ec4be3f3eec57d5a3a9f29709b171
Shivansh Sharma <shivanshsharma82191@gmail.com>
2026-05-10 22:38:17 +0530

    Added odd number count program

---
+++ CountOddNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CountOddNumbers {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter how many numbers: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+
+            System.out.print("Enter number " + i + ": ");
+            int num = sc.nextInt();
+
+            if (num % 2 != 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Total odd numbers = " + count);
+
+        sc.close();
+    }
+}
