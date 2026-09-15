88e096a78b8b0d996867c336c577d9ceca4d206b
Shivansh Sharma <shivanshsharma82191@gmail.com>
2026-09-15 23:51:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Traversal/array-traversal.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void arrayTraversal(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i > 0) {
+                System.out.print(" ");
+            }
+            System.out.print(arr[i]);
+        }
+    }
+}
