8daf16f1f383d129c1c595ccf5bc92591d4ea051
Shivansh Sharma <shivanshsharma82191@gmail.com>
2026-09-21 14:54:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.java
@@ -0,0 +1,11 @@
+class Solution {
+    long factorial(int n) {
+        long fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        return fact;
+    }
+}
