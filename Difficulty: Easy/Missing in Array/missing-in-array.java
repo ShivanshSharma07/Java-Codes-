5ec38360711916c970c14b423cb13e2c8d52159d
Shivansh Sharma <shivanshsharma82191@gmail.com>
2026-09-21 14:40:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    int missingNum(int[] arr) {
+        int n = arr.length + 1;
+        int xor = 0;
+
+   
+        for (int i = 1; i <= n; i++) {
+            xor ^= i;
+        }
+
+    
+        for (int num : arr) {
+            xor ^= num;
+        }
+
+        return xor;
+    }
+}
